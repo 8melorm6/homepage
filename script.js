372b67d9d0817e4590f0d94479c48a009e41db62
Michael Rommel <melorm@posteo.de>
2025-10-13 13:34:17 +0200

    Überarbeitung der Grundstruktur

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     meldung.textContent = "Nachricht wurde lokal gesendet (Demo).";
+    meldung.style.color = "#92fe9d";
     form.reset();
   });
 });
